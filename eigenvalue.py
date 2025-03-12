d2249d7460911f8f78840e98be7665708860220c
arvberg <arvidbmobil@gmail.com>
2025-03-12 21:50:57 +0100

    Klar, identiska resultat.

--- eigenvalue.py
+++ eigenvalue.py
@@ -1,48 +1,55 @@
 import numpy as np
 import numpy.linalg as LA
 
-def rayleigh(B, b) -> float:
-    B = np.ndarray(B)
-    b = np.ndarray(b)
-
-    """Ser så att B är kvadratisk, att B och b har en delad dimension n eller m, och att b är normalvektor."""
+def rayleigh(B, b):
+    # Ensure B is square, B and b have matching dimensions, and b is a unit vector.
     if B.shape[0] != B.shape[1]:
         raise ValueError('B is not square.')
     if B.shape[0] != b.shape[0]:
         raise ValueError('B and b must share dimensions.')
-    if np.linalg.norm(b) != 1.0:
+    if not np.isclose(LA.norm(b), 1.0):
         raise ValueError('b is not a normal vector (length of 1).')
 
-    """b^T * B * b med matriser enligt numpydokumentation."""
-    return float(b.T @ B @ b)
+    return b.T @ B @ b # @ is matrix multiplication, .T is transposition of a matrix.
 
 
-
-def max_eigenvalue(B: np.ndarray, p: int):
+def max_eigenvalue(B, p):
     if B.shape[0] != B.shape[1]:
         raise ValueError('B is not square.')
 
-    n = B.shape[0]
-    b = np.random.randn(n, p) # random matrix with n rows, p columns
-    b = LA.norm(b)
+    b = np.ones((B.shape[0], 1))  # Initialize b as column vector of ones for guessed eigenvector
+    b /= LA.norm(b)  # Fix: Normalize b at initialization
+    prev_rayleigh = rayleigh(B, b) # Initial eigenvalue guess
+    tolerance = 10 ** -p
 
-    prev_rayleigh = rayleigh(B, b)
-    tolerance = 10**-p
-
-    """Jämför med flera följande på varandra Rayleigh-kvoter för varje iteration av B."""
     while True:
-        next_b = B @ b # [B][b]
-        next_b /= LA.norm(next_b) # [b]/|[b]|
+        next_b = B @ b # [b] = [B][b]
+        next_b /= LA.norm(next_b)  # [b] = [b]/|[b]|
 
-        curr_rayleigh = rayleigh(B, next_b)
+        curr_rayleigh = rayleigh(B, next_b) # Iterations of Rayleigh
 
         if abs(curr_rayleigh - prev_rayleigh) < tolerance:
-            return curr_rayleigh, next_b
+            return next_b, curr_rayleigh
 
         prev_rayleigh = curr_rayleigh
         b = next_b
 
+# a)
+print("a)")
+B = np.array([[9, 5],
+              [1, 5]])
 
-# (A)
-B = np.array([[9, 5], [1, 5]])
+eigenvector, eigenval = max_eigenvalue(B, 6)
+print("Largest eigenvalue through power iteration: ", round(eigenval[0,0], 2))
+print("The largest eigenvalue through LA.eig: ", max(LA.eig(B)[0]))
 
+# b)
+print("\nb)")
+A = np.random.rand(500, 500)
+B = A + A.T  # Make B symmetric
+
+v, eigenvalue = max_eigenvalue(B, 6)
+magnitude = LA.norm(B @ v - eigenvalue * v)  # Should be close to zero
+
+print("Largest eigenvalue for B:", round(eigenvalue[0,0], 2))
+print("Bv - λv = ", magnitude)
